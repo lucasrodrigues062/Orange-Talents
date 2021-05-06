be09db6ec07290781738480ab89817c67bd90ee1
Lucas Rodrigues <lucasrodrigues062@gmail.com>
2021-05-06 08:36:56 -0300

    Entregue avaliação de Servlets

--- Curso 1 - Java Servlets/gerenciador/src/main/java/br/com/zup/gerenciador/servlet/NovaEmpresaServlet.java
+++ Curso 1 - Java Servlets/gerenciador/src/main/java/br/com/zup/gerenciador/servlet/NovaEmpresaServlet.java
@@ -45,7 +45,7 @@ public class NovaEmpresaServlet extends HttpServlet {
 		Banco banco = new Banco();
 		banco.adiciona(empresa);
 		
-		request.setAttribute("empresa", empresa.getNome());
+		//request.setAttribute("empresa", empresa.getNome());
 		response.sendRedirect("listaEmpresa");
 		
 		
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Orange-Talents
 Repositório com todo conteúdo do Orange Talents Turma #5
+
+[x] Curso - Java Servlet: Fundamentos da programação web Java.
+[] HTTP: Entendendo a web por baixo dos panos.
